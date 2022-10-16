ba83b8402b5aa11068692b1cbfc7089086283e46
Amr Hammoud <amrhammoud.dev@gmail.com>
2022-10-16 14:08:37 +0300

    First App

---
+++ W1-1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void display(View v) {
+        Toast.makeText(this, "End of Lecture", Toast.LENGTH_SHORT).show();
+    }
+
+}
